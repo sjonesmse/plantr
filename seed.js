cc10d98c07db3492d0e5e409c936ae4bc0d3a14c
Sandy Jones <sjonesmse@gmail.com>
2019-01-22 16:29:36 -0600

    so many vegetables

--- seed.js
+++ seed.js
@@ -2,7 +2,33 @@ const { db, Gardener, Plot, Vegetable } = require('./models');
 
 db.sync({ forced: true })
     .then(() => {
+        const vegetablesPromise = Vegetable.bulkCreate([
+            {
+                name: 'Cabbage',
+                color: 'Purple',
+                planted_on: '2016-08-09 04:05:02'
+            },
+            {
+                name: 'Carrot',
+                color: 'Orange',
+                planted_on: '2016-08-09 04:05:02'
+            },
+            {
+                name: 'Potato',
+                color: 'Golden',
+                planted_on: '2016-08-09 04:05:02'
+            },
+            {
+                name: 'Broccoli',
+                color: 'Green',
+                planted_on: '2016-08-09 04:05:02'
+            }
+        ], { returning: true });
         console.log("Database is synctified!");
+
+
+        return Promise.all(vegetablesPromise);
+
     })
     .catch((err) => {
         console.error(err);
